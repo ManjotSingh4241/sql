216048bc95e975d2d8e2c74922b50670008bea31
Manjot singh <82448988+ManjotSingh4241@users.noreply.github.com>
2023-02-23 07:18:24 -0500

    Add files via upload

--- day1/cancatenation.sql
+++ day1/cancatenation.sql
@@ -1,1 +1,10 @@
+--Concatination
 select '+1 '||phone as "Phone Number" from employees; -- It helps to join two columns
+select first_name||' is a '||job_title as "employee details" from employees;
+
+--Duplicate Rows
+select job_title from employees;
+select distinct job_title from employees;
+
+--Displaying table structure
+DESCRIBE employees;
